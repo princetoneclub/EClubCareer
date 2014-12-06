8e098908401951ebcb486446bdd0026c507da8d3
Irvin Zhan <izhan@princeton.edu>
2014-12-05 20:45:04 -0500

    fixing rspec to run

--- spec/controllers/postings_controller_spec.rb
+++ spec/controllers/postings_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe PostingsController, :type => :controller do
+describe "Postings Pages" do
+  subject { page }
 
+  describe "#new" do
+    before { visit new_posting_path }
+
+    let(:submit) { "submit" }
+
+    describe "with valid information" do
+      before do
+        fill_in "Title", with: "Random Title"
+        fill_in "Location", with: "New Jersey"
+        fill_in "Description", with: "Sample Description"
+        fill_in "Deadline", with: Time.now()
+        fill_in "Type", with: "Internship"
+      end
+      
+      it "should create a posting" do
+        expect { click_button submit }.to change(Posting, :count)
+      end
+    end
+  end
 end
