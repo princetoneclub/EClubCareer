55d551a4231246eea0a725a95dddbcc0eeb10722
Advait Chauhan <achauhan@princeton.edu>
2014-12-12 17:56:54 -0500

    Sorted postings list, reduced descriptions, added deadline filter

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -2,6 +2,7 @@ class Posting < ActiveRecord::Base
   validates_presence_of :title, :description, :location, :ptype # :deadline # TODO
 	def self.search(params)
 		postings = self.all
+		postings = postings.where("deadline > ?", Time.now).order(:deadline)
 	  	postings = postings.where("title like ? ", "%#{params[:search]}%") if params[:search].present?
 	  	postings = postings.where("ptype like ? ", "%#{params[:ptype]}%") if params[:ptype].present?
 		return postings
