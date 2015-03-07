6835293d1de1c83ab20451ea0501f59c71885350
Yash <yashpatel5400@gmail.com>
2015-03-06 19:28:40 -0800

    refactored code from display

--- app/helpers/posting_helper.rb
+++ app/helpers/posting_helper.rb
@@ -1,2 +1,13 @@
 module PostingHelper
+	def get_opportunity_type(ptype)
+		if ptype == "full_time"
+			return "Full Time"
+						
+		elsif ptype == "part_time"
+			return "Part Time"
+						
+		else ptype == "internship"
+			return "Internship"	
+		end 
+	end
 end
