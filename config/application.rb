3b5ce596e583cb2db503f86c03dd47744393fcc5
Kavinayan <kavinayan.sivakumar@gmail.com>
2015-03-28 16:58:18 -0400

    changed some stuff per irvin's request

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module EclubCareer
   class Application < Rails::Application
-    config.assets.enabled = true
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
