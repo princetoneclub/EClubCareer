100dc85e412091cdfed050eb7bf810b56e120389
Richard Chu <richardchu10@gmail.com>
2015-04-04 17:37:13 -0400

    Add search by parameters

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -3,14 +3,19 @@ class Posting < ActiveRecord::Base
   validate :check_ptype
 
   def check_ptype
-    errors.add(:ptype, "Cannot be this value") if (self.ptype != "full_time" && self.ptype != "part_time" && self.ptype != "internship")
+  	errors.add(:ptype, "Cannot be this value") if (self.ptype != "full_time" && self.ptype != "part_time" && self.ptype != "internship")
   end
- 
-	def self.search(params)
-		postings = self.all
-		postings = postings.where("deadline > ?", Time.now).order(:deadline)
-	  	postings = postings.where("title like ? ", "%#{params[:title]}%") if params[:title].present?
-	  	postings = postings.where("ptype like ? ", "%#{params[:ptype]}%") if params[:ptype].present?
-		return postings
-	end
+  
+  def self.search(params)
+  	postings = self.all
+  	postings = postings.where("deadline > ?", Time.now).order(:deadline)
+  	postings = postings.where("title like ? ", "%#{params[:title]}%") if params[:title].present?
+  	postings = postings.where("location like ? ", "%#{params[:location]}%") if params[:location].present?
+  	if params[:ptype].present?
+  	  postings = postings.where("ptype != 'full_time'") if params[:ptype]["full_time"] == "0"
+  	  postings = postings.where("ptype != 'part_time'") if params[:ptype]["part_time"] == "0"
+  	  postings = postings.where("ptype != 'internship'") if params[:ptype]["internship"] == "0"
+  	end
+  	return postings
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
 
      get 'confirmation' => 'static_pages#confirmation'
      get 'submit' => 'postings#new'
-     get 'search' => 'postings#search'
      
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
